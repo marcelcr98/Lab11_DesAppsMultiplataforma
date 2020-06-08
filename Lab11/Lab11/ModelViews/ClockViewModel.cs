cbfd1bfb65f7e8caa84729e248837ee6b5d143db
marceloarmando <mcuellarr98@gmail.com>
2020-06-08 14:51:02 -0500

    commit Marcelo

---
+++ Lab11/Lab11/ModelViews/ClockViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Lab11.ModelViews
+{
+    class ClockViewModel : INotifyPropertyChanged
+    {
+        DateTime dateTime;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ClockViewModel()
+        {
+            this.DateTime = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+
+        public DateTime DateTime
+        {
+            set
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+            get
+            {
+                return dateTime;
+            }
+        }
+
+    }
+}
